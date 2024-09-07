415c6641f903e7a1b54e49ac2e54ada86e9a846c
Amirreza Yazdanpanah <amirgta98@gmail.com>
2024-09-08 00:23:20 +0330

    edit  folder

--- temp/dist/output.js
+++ temp/dist/output.js
@@ -1,1 +1,1 @@
-const htmlFiles = ["E:/work/main project\\index.html", "E:/work/main project\\pos\\pos.html", "E:/work/main project\\pos\\pos2.html", "E:/work/main project\\pos\\pos3.html", "E:/work/main project\\QTv\\qtv-2product.html", "E:/work/main project\\QTv\\qtv-productList.html"];
+const htmlFiles = ["E:/work/main project\\index.html", "E:/work/main project\\work space\\Qtv\\ads.html", "E:/work/main project\\work space\\Qtv\\qtv-2product.html", "E:/work/main project\\work space\\Qtv\\qtv-productList.html"];
