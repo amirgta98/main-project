bcbf0075988d37e313c52a6adbfed05f21ea2d29
Amirreza Yazdanpanah <amirgta98@gmail.com>
2025-01-17 13:23:11 +0330

    change all qtv and optimize script code

--- asset/script/general.js
+++ asset/script/general.js
@@ -1,174 +1,323 @@
-console.log("script run")
-var direction =  $("body");
-const ScripCdn = $("#Script-Cdn")
-const CssCdn = $("#Css-Cdn")
-
-if(direction.hasClass("go-rtl")){
-    direction.css("direction","rtl")
-
-    ScripCdn.attr("scr","https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js")
-    ScripCdn.attr("integrity","sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4")
-    CssCdn.attr("scc","https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css")
-    CssCdn.attr("integrity","sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb")
+/**
+ * Main Configuration and Initialization
+ * Handles RTL/LTR direction setup and CDN configurations
+ */
+$(document).ready(function() {
 
 
+    // Helper function to setup direction and CDN attributes
+    function setupDirection(dir, config) {
+        direction.css("direction", dir);
+        scriptCdn.attr({
+            "scr": config.script,
+            "integrity": config.scriptIntegrity
+        });
+        cssCdn.attr({
+            "scc": config.css,
+            "integrity": config.cssIntegrity
+        });
+    }
+});
 
-}else if(direction.hasClass("go-ltr")){
-    direction.css("direction","ltr")
-    ScripCdn.attr("scr","https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js")
-    ScripCdn.attr("integrity","sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz")
-    CssCdn.attr("scc","https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css")
-    CssCdn.attr("integrity","sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65")
-}else{
-    console.error("please add class go-ltr or go-rtl to body")
-}
-
-// numberic up down
-$(".nud-container").each(function (indexInArray, valueOfElement) { 
-    var plus = $(valueOfElement).find(".nud-plus");
-    var number = $(valueOfElement).find(".nud-num h5");
-    var mines = $(valueOfElement).find(".nud-mines");
-
-    // افزودن event handler برای دکمه ی plus
-    plus.on("click", function() {
-        var currentNumber = parseInt(number.text());
-        number.text(currentNumber + 1);
+/**
+ * Numeric Up/Down Counter Implementation
+ * Handles increment/decrement functionality for product quantities
+ */
+$(".nud-container").each(function() {
+    const container = $(this);
+    const number = container.find(".nud-num h5");
+    
+    // Increment handler
+    container.find(".nud-plus").click(function() {
+        const currentValue = parseInt(number.text());
+        number.text(currentValue + 1);
     });
-
-    // افزودن event handler برای دکمه ی mines
-    mines.on("click", function() {
-        var currentNumber = parseInt(number.text());
-        if (currentNumber > 1) {
-            number.text(currentNumber - 1);
-        }else{
-            $(valueOfElement).closest(".I-prudact").addClass("d-none");
+    
+    // Decrement handler with product removal
+    container.find(".nud-mines").click(function() {
+        const currentValue = parseInt(number.text());
+        if (currentValue > 1) {
+            number.text(currentValue - 1);
+        } else {
+            container.closest(".I-prudact").addClass("d-none");
         }
     });
 });
-$(".I-prudact").each(function (indexInArray, valueOfElement){
-    var itemopener =  $(valueOfElement).find(".product-list-items-text");
-    var items = $(valueOfElement).find(".product-items");
-    itemopener.click(function(){
-        items.toggleClass("d-none");        
-    })
-})
-$(".k-Btn").click(function(){
-    $(".k-Btn").removeClass("pay-K-Btn-selected")
-    $(this).addClass("pay-K-Btn-selected")
-})
 
-// open sub catgury 
-$(".catgury-container").each(function (indexInArray, valueOfElement) { 
-    var subOpener =  $(valueOfElement).find(".catgury-open-sub-cat")   
-    var opened = false;    
-    subOpener.click(function(){
-        if(opened){
-            $(this).removeClass("catgury-open-sub-cat-rotated")
-            $(valueOfElement).find(".catgury-open-sub-cat-box").addClass("d-none")
-            opened = false
-        }
-        else{
-            $(".catgury-open-sub-cat").removeClass("catgury-open-sub-cat-rotated")
-            $(".catgury-container .catgury-open-sub-cat-box").addClass("d-none")
-
-            $(this).addClass("catgury-open-sub-cat-rotated")
-            $(valueOfElement).find(".catgury-open-sub-cat-box").removeClass("d-none")
-            opened = true;
-        }
-
-
-    })    
-});
-// collapse script
-$(".collapse-containe").each(function (indexInArray, valueOfElement) {
-    $(valueOfElement).find('.colapse-Btn-container button').on('click', function() {
-        $(valueOfElement).find(".colapse-Btn-container button").removeClass("btn-style-selected")
-        $(this).addClass("btn-style-selected")
-        var target = $(this).attr('target');
-        $(valueOfElement).find('.collapse-content').addClass('d-none'); // Hide all contents
-        $(valueOfElement).find('#' + target).removeClass('d-none'); // Show the targeted content
+/**
+ * Product List Item Toggle Implementation
+ * Handles showing/hiding product items
+ */
+$(".I-prudact").each(function() {
+    const product = $(this);
+    product.find(".product-list-items-text").click(function() {
+        product.find(".product-items").toggleClass("d-none");
     });
-})
+});
 
-$(".collapse-content").each(function (indexInArray, valueOfElement) { 
-    $(valueOfElement).find(".pos-address-opener").click(function(){
-        $(valueOfElement).find(".pos-adressBoxcontainer").toggleClass("d-none")
-    })
-})
-// pos product show
-var windowSize = $(".us-product").outerHeight(); 
-var productSize = $(".us-L-procudt").outerHeight();
+/**
+ * Payment Button Selection Handler
+ */
+$(".k-Btn").click(function() {
+    $(".k-Btn").removeClass("pay-K-Btn-selected");
+    $(this).addClass("pay-K-Btn-selected");
+});
 
- if(productSize>=windowSize){
-    $(".us-R-procudt").removeClass("d-none")
-    $(".us-product").addClass("us-product-lot")
-    var productSize2 = $(".us-R-procudt").outerHeight()
-    function scrollToBottom() {
-        $('.us-product').animate({ scrollTop: $('.us-product')[0].scrollHeight }, 4000, scrollToTop);
+/**
+ * Category Navigation Implementation
+ * Handles subcategory display toggling
+ */
+$(".catgury-container").each(function() {
+    const container = $(this);
+    const subOpener = container.find(".catgury-open-sub-cat");
+    let isOpen = false;
+    
+    subOpener.click(function() {
+        const subCatBox = container.find(".catgury-open-sub-cat-box");
+        
+        if (isOpen) {
+            subOpener.removeClass("catgury-open-sub-cat-rotated");
+            subCatBox.addClass("d-none");
+        } else {
+            // Close all other open categories
+            $(".catgury-open-sub-cat").removeClass("catgury-open-sub-cat-rotated");
+            $(".catgury-container .catgury-open-sub-cat-box").addClass("d-none");
+            
+            // Open current category
+            subOpener.addClass("catgury-open-sub-cat-rotated");
+            subCatBox.removeClass("d-none");
+        }
+        isOpen = !isOpen;
+    });
+});
+
+/**
+ * Collapse Implementation
+ * Handles content switching in collapse containers
+ */
+$(".collapse-containe").each(function() {
+    const container = $(this);
+    
+    container.find('.colapse-Btn-container button').click(function() {
+        const button = $(this);
+        const target = button.attr('target');
+        
+        container.find(".colapse-Btn-container button").removeClass("btn-style-selected");
+        button.addClass("btn-style-selected");
+        
+        container.find('.collapse-content').addClass('d-none');
+        container.find('#' + target).removeClass('d-none');
+    });
+});
+
+/**
+ * Address Box Toggle Implementation
+ */
+$(".collapse-content").each(function() {
+    const content = $(this);
+    content.find(".pos-address-opener").click(function() {
+        content.find(".pos-adressBoxcontainer").toggleClass("d-none");
+    });
+});
+
+/**
+ * Product Display Animation
+ * Implements auto-scroll functionality for product display
+ */
+function initializeProductScroll() {
+    const windowSize = $(".us-product").outerHeight();
+    const productSize = $(".us-L-procudt").outerHeight();
+    
+    if (productSize >= windowSize) {
+        $(".us-R-procudt").removeClass("d-none");
+        $(".us-product").addClass("us-product-lot");
+        
+        function scrollToBottom() {
+            $('.us-product').animate(
+                { scrollTop: $('.us-product')[0].scrollHeight }, 
+                4000, 
+                scrollToTop
+            );
+        }
+        
+        function scrollToTop() {
+            $('.us-product').animate(
+                { scrollTop: 0 }, 
+                4000, 
+                scrollToBottom
+            );
+        }
+        
+        scrollToBottom();
     }
-
-    function scrollToTop() {
-        $('.us-product').animate({ scrollTop: 0 }, 4000, scrollToBottom);
-    }
-
-    scrollToBottom();
- }
-//  nav space maker
-// we have 2 way for make space of nav for end of screen
-// first: if wa want put a extra space to end od div that parent dose not have flex style 
-
-$(".nav-item").click(function(){
-    $(".nav-item").removeClass("nav-item-Selected")
-    $(this).addClass("nav-item-Selected")
-}) 
-
-
-function navigetionsize(nav,navSpaceMaker){
-    var navigationHeight = $(nav).outerHeight();
-    navigationHeight = navigationHeight*1.2;
-    $(navSpaceMaker).outerHeight(navigationHeight)
 }
-navigetionsize(".pos-navigetion-container",".nav-space-maker")
-navigetionsize(".checkoutBtn",".kiosk-space-maker")
 
+initializeProductScroll();
 
-// nav control
-$('.pos-modal-container').on('click', function(event) {
-    console.log(12)
-    // اگر کلیک خارج از pos-modal باشد
+/**
+ * Navigation Implementation
+ * Handles navigation item selection and space calculation
+ */
+$(".nav-item").click(function() {
+    $(".nav-item").removeClass("nav-item-Selected");
+    $(this).addClass("nav-item-Selected");
+});
+
+// Navigation space calculation helper
+function calculateNavigationSpace(nav, spaceMaker) {
+    const navigationHeight = $(nav).outerHeight() * 1.2;
+    $(spaceMaker).outerHeight(navigationHeight);
+}
+
+// Initialize navigation spaces
+calculateNavigationSpace(".pos-navigetion-container", ".nav-space-maker");
+calculateNavigationSpace(".checkoutBtn", ".kiosk-space-maker");
+
+/**
+ * Modal and Navigation Control Implementation
+ */
+$('.pos-modal-container').click(function(event) {
     if (!$(event.target).closest('.pos-modal').length) {
         $(this).addClass('d-none');
-        $(".nav-item").removeClass("nav-item-Selected")
-        $('.nav-item[target="pos-home-page"]').addClass("nav-item-Selected")
-
+        $(".nav-item").removeClass("nav-item-Selected");
+        $('.nav-item[target="pos-home-page"]').addClass("nav-item-Selected");
     }
 });
-$(".nav-container .nav-item").each(function (indexInArray, valueOfElement) { 
-    $(valueOfElement).click(function(){
-        var NavbBtnTarget = $(this).attr("target")
-        $("#"+NavbBtnTarget).removeClass("d-none")
-        if(NavbBtnTarget =="pos-total-page"){
-          $(".pos-product-unselected-container").removeClass("d-none")            
-          $(".pos-product-selected-container").addClass("d-none")  
+
+// Navigation item click handlers
+$(".nav-container .nav-item").click(function() {
+    const targetId = $(this).attr("target");
+    const target = $("#" + targetId);
+    
+    target.removeClass("d-none");
+    
+    if (targetId === "pos-total-page") {
+        $(".pos-product-unselected-container").removeClass("d-none");
+        $(".pos-product-selected-container").addClass("d-none");
+    }
+});
+
+// Back button handler
+$(".pos-back-btn").click(function() {
+    $(".pos-product-unselected-container").addClass("d-none");
+    $(".pos-product-selected-container").removeClass("d-none");
+    $(".nav-item").removeClass("nav-item-Selected");
+    $('.nav-item[target="pos-home-page"]').addClass("nav-item-Selected");
+});
+
+/**
+ * Creates a typing effect for the given text
+ * @param {string} text - The text to be typed
+ * @param {string} selector - jQuery selector for target element
+ * @param {object} options - Configuration options
+ */
+function typeEffect(text, selector, options = {}) {
+    const settings = {
+        speed: options.speed || 100,
+        delay: options.delay || 0,
+        onComplete: options.onComplete || function() {}
+    };
+
+    const element = $(selector);
+    
+    if (!element.length) {
+        console.error(`Element "${selector}" not found`);
+        return;
+    }
+
+    // ایجاد یک المان موقت و جایگزینی &nbsp; با فاصله عادی
+    const tempDiv = $('<div>').html(text);
+    const processedText = tempDiv.html().replace(/&nbsp;/g, ' ');
+    
+    let currentHtml = '';
+    let index = 0;
+    element.empty();
+
+    setTimeout(() => {
+        const typeInterval = setInterval(() => {
+            if (index < processedText.length) {
+                // اگر کاراکتر جاری بخشی از یک تگ HTML است
+                if (processedText[index] === '<') {
+                    let tagContent = '<';
+                    index++;
+                    
+                    // جمع‌آوری کل تگ
+                    while (index < processedText.length && processedText[index] !== '>') {
+                        tagContent += processedText[index];
+                        index++;
+                    }
+                    if (index < processedText.length) {
+                        tagContent += '>';
+                        currentHtml += tagContent;
+                        index++;
+                    }
+                } else {
+                    currentHtml += processedText[index];
+                    index++;
+                }
+                
+                element.html(currentHtml);
+            } else {
+                clearInterval(typeInterval);
+                settings.onComplete();
+            }
+        }, settings.speed);
+    }, settings.delay);
+}
+
+
+/**
+ * Animates a number counting up to a target value
+ * @param {string} selector - jQuery selector for the target element
+ * @param {object} options - Configuration options
+ * @param {number} options.speed - Animation duration in milliseconds
+ * @param {number} options.delay - Initial delay before animation starts
+ * @param {string} options.suffix - Text to append after the number (default: 'cal')
+ * @param {function} options.onComplete - Callback function when animation completes
+ */
+function countUp(selector, options = {}) {
+    const settings = {
+        speed: options.speed || 1000,
+        delay: options.delay || 0,
+        suffix: options.suffix || 'cal',
+        onComplete: options.onComplete || function() {}
+    };
+
+    const element = $(selector);
+    const destination = parseInt(element.text().replace(/\D/g, ''));
+    
+    if (isNaN(destination)) {
+        console.error('Invalid number format');
+        return;
+    }
+
+    // Set initial value
+    element.text(`0${settings.suffix}`);
+
+    // Wait for specified delay
+    setTimeout(() => {
+        const startTime = performance.now();
+        
+        // Use requestAnimationFrame for smooth animation
+        function animate(currentTime) {
+            const elapsed = currentTime - startTime;
+            const progress = Math.min(elapsed / settings.speed, 1);
+
+            // Use easeOutQuad for smoother animation
+            const easeProgress = 1 - (1 - progress) * (1 - progress);
+            const currentValue = Math.round(destination * easeProgress);
+
+            element.text(`${currentValue}${settings.suffix}`);
+
+            if (progress < 1) {
+                requestAnimationFrame(animate);
+            } else {
+                // Ensure final value is exact
+                element.text(`${destination}${settings.suffix}`);
+                settings.onComplete();
+            }
         }
 
-        $("#"+NavbBtnTarget).removeClass("d-none")
-        
-    })
-     
-});
-$(".pos-back-btn").click(function(){
-    
-    $(".pos-product-unselected-container").addClass("d-none")
-    $(".pos-product-selected-container").removeClass("d-none")  
-    $(".nav-item").removeClass("nav-item-Selected")
-    $('.nav-item[target="pos-home-page"]').addClass("nav-item-Selected")         
-
-})
-
-
-
-
-
-
-
+        requestAnimationFrame(animate);
+    }, settings.delay);
+}
